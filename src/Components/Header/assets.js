598dfdab43e7cbad82b907dceef34cb154d17ceb
Abdeali vora <abdalivora786@gmail.com>
2023-02-17 03:40:18 +0530

    First Phase Done 90%

---
+++ src/Components/Header/assets.js
@@ -0,0 +1,1 @@
+export const demoProfile = require("../../assets/demoProfile.jpg");
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+const Home = () => {
+  return (
+    <div className="flex max-w-full">
+      <div className="bg-black w-1/6">
+        <Header />
+      </div>
+      <div className="w-5/6">Comming Soon Baby</div>
+    </div>
+  );
+};
+export default Home;
